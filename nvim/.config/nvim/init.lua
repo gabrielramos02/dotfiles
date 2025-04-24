8c1fceb24ca6a463601d5c3977ebe310687a2b12
gabrielrp02 <gabrielrp02ok@outlook.com>
2025-04-24 13:16:11 +0200

    added tokyo-night theme to lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,23 +1,21 @@
-
 --> PLUGINS ------------------------------------------------------ {{{
-vim.g.loaded_netrw       = 1
+vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 if vim.g.vscode then
-    -- VSCode extension
---	vim.opt.rnu = true
---	vim.opt.nu = true
+	-- VSCode extension
+	--	vim.opt.rnu = true
+	--	vim.opt.nu = true
 	vim.cmd.filetype("on")
 	vim.cmd.filetype("plugin on")
 	vim.cmd.filetype("indent on")
 	vim.cmd.syntax("on")
-   	require("config.lazy")
-	require('custom.remap')
+	require("config.lazy")
+	require("custom.remap")
 	-->require('coc')
---	require('undotree').setup()
+	--	require('undotree').setup()
 else
-    -- ordinary Neovim
-	require('custom')
+	-- ordinary Neovim
+	require("custom")
 	require("config.lazy")
 end
 --
-
