b304c2c4a465bee5ed5b53bb1b41f716397c12e1
gabrielramos02 <gabrielrp02ok@outlook.com>
2026-02-02 18:21:49 +0100

    updated 02.02.26

--- nvim/.config/nvim/lua/plugins/flutter-tools.lua
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -12,6 +12,15 @@ return {
 				open_cmd = "30vnew", -- command to use to open the outline buffer
 				auto_open = false, -- if true this will open the outline automatically when it is first populated
 			},
+			dev_log = {
+				enabled = true,
+				filter = nil, -- optional callback to filter the log
+				-- takes a log_line as string argument; returns a boolean or nil;
+				-- the log_line is only added to the output if the function returns true
+				notify_errors = false, -- if there is an error whilst running then notify the user
+				open_cmd = "belowright 15new", -- command to use to open the log buffer
+				focus_on_open = true, -- focus on the newly opened log window
+			},
 		})
 		-- Telescope flutter
 		require("telescope").load_extension("flutter")
